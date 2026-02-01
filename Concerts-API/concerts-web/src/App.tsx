056164d383d86adc6d6f0763e3f6d81d2cabd753
Housanoss <vojta@houskovi.cz>
2026-02-01 23:57:19 +0100

    bug fix3

--- Concerts-API/concerts-web/src/App.tsx
+++ Concerts-API/concerts-web/src/App.tsx
@@ -1,33 +1,3 @@
-﻿import { useEffect, useState } from 'react';
-import "./App.css"; 
-
-// 1. Define the shape of your data. 
-// This must match your C# Concert class properties exactly.
-export interface Concert {
-    id: number;      
-    venue: string;
-    date: string;         
-    price: number;
-    genres: string;
-    description: string;
-    sold_out: boolean;  
-
-    bands: string;
-    headliner: string;
-    openers: string;
-}
-
-export interface Ticket {
-    id: number;
-    concertId: number; 
-    userId: number;     
-    price: number;
-    description: string;
-    concert?: Concert; //connection to concert to know name
-}
-
-
-
-
+﻿
 
   
